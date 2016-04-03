68e08fab1bfd0afda4590f769d0fd5d9a4ae017e
Charlie Greenman <charie@razroo.com>
2016-04-03 01:58:15 -0400

    Refactor color picker component to re-use jsx compoenents. Add in classnames module, for more readable jsx compoenents.

--- app/js/components/color-picker.js
+++ app/js/components/color-picker.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { connect, Provider } from "react-redux";
+import classNames from "classnames";
+
+class ColorPickerColumn extends React.Component{
+  render(){
+    return(
+      <div className="color-picker__row__column">
+          <label className="color-picker__row__column__label" >{this.props.letter}</label>
+          <input className={classNames("color-picker__row__column__input", this.props.inputClass)} id={this.props.inputId} type="text" maxLength={3} defaultValue={25} />
+      </div>
+    )
+  }
+}
 
 class ColorPicker extends React.Component {
   constructor(props){
@@ -23,26 +35,18 @@ class ColorPicker extends React.Component {
   render() {
     const {dispath} = this.props;
       return (
+        <div>
           <div className="color-picker" id="background-color-picker">
             <h3 className = "color-picker__header">Background Color</h3>
             <label className = "color-picker__label" id="hex_label">Hex</label>
             <input className = "color-picker__color" id="background-hex-color" type="text" defaultValue={this.state.backgroundHex}  onChange={this.handleBackgroundColorChange} maxLength={7} className="large-12" />
-            //turn all of these into componenents for easy re-use and change into props 
             <div id="background_color_bar" className="color-picker__color-bar" />
-              <div id="rgb-background" className="color-picker__row">
-                <div className="color-picker__row__column">
-                    <label className="color-picker__row__column__label" >R </label>
-                    <input className="color-picker__row__column__input backgroundRgb" id="background-red" type="text" maxLength={3} defaultValue={25} />
-                </div>
-                <div className="color-picker__row__column">
-                  <label className="color-picker__row__column__label" >G</label>
-                  <input className="color-picker__row__column__input backgroundRgb" id="background-green" type="text" maxLength={3} defaultValue={25} />
-                </div>
-                <div className="color-picker__row__column">
-                  <label className="color-picker__row__column__label" >B</label>
-                  <input className="color-picker__row__column__input backgroundRgb" id="background-blue" type="text" maxLength={3} defaultValue={25} />
-                </div>
-              </div>
+            <div id="rgb-background" className="color-picker__row">
+              <ColorPickerColumn letter = "R" inputClass = "backgroundRgb" inputId = "background-red" />
+              <ColorPickerColumn letter = "G" inputClass = "backgroundRgb" inputId = "background-green" />
+              <ColorPickerColumn letter = "B" inputClass = "backgroundRgb" inputId = "background-blue" />
+            </div>
+          </div>
 
           <div id="color-picker" className="column large-12">
             <h3 className = "color-picker__header">Pixel Color</h3>
@@ -50,21 +54,12 @@ class ColorPicker extends React.Component {
             <input id="pix-hex-color" type="text" defaultValue={this.state.pixelHex} onChange={this.handlePixelColorChange} maxLength={7} className = "color-picker__color" />
             <div id="color_bar" className="large-12" />
             <div id="rgb" className="color-picker__row">
-              <div className="color-picker__row__column">
-                  <label className="color-picker__row__column__label" >R </label>
-                  <input className="color-picker__row__column__input backgroundRgb" id="background-red" type="text" maxLength={3} defaultValue={25} />
-              </div>
-              <div className="color-picker__row__column">
-                <label className="color-picker__row__column__label" >G</label>
-                <input className="color-picker__row__column__input backgroundRgb" id="background-green" type="text" maxLength={3} defaultValue={25} />
-              </div>
-              <div className="color-picker__row__column">
-                <label className="color-picker__row__column__label" >B</label>
-                <input className="color-picker__row__column__input backgroundRgb" id="background-blue" type="text" maxLength={3} defaultValue={25} />
-              </div>
-              </div>
+              <ColorPickerColumn letter = "R" inputClass = "backgroundRgb" inputId = "background-red" />
+              <ColorPickerColumn letter = "G" inputClass = "backgroundRgb" inputId = "background-green" />
+              <ColorPickerColumn letter = "B" inputClass = "backgroundRgb" inputId = "background-blue" />
+            </div>
           </div>
-        </div>
+        </div>  
         )
       }
 }
