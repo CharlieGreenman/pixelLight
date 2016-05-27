ce9869d1ea6128a8b6b8663431fb90fe7d8c1181
Charlie Greenman <charie@razroo.com>
2016-05-27 01:29:10 -0400

    In process of adding array map to js code box conversion.

--- app/js/components/code-box/js-view/js-arr-map.js
+++ app/js/components/code-box/js-view/js-arr-map.js
@@ -7,8 +7,26 @@ class JSArrMap extends React.Component {
     super();
   }
   render(){
+    const{rowCount, columnCount, pixelCount, xPos, yPos, rectColor} = this.props;
+    let colorStore     = _.uniq(rectColor);
+    let arrMap = [];
+    for (let x = 0; x < rowCount; x++) {
+        arrMap.push([]);
+    }
+    for (let x = 0; x < rowCount; x++) {
+        for (let y = 0; y < columnCount; y++) {
+            arrMap[y].push(0);
+            for (let z = 0; z < xPos; z++) {
+                //tells us value needs to be changed
+                if (x === parseFloat(xPos[z] / pixelCount) && y === parseFloat(yPos[z] / pixelCount)) {
+                    //tells us what it should be changed to
+                    arrMap[y][x] = elem.s.storeColors.indexOf(rectColor[z]) + 1;
+                }
+            }
+        }
+    }
     return(
-      <div> works as expected </div>
+      <div> {arrMap} </div>
     );
   }
 }
