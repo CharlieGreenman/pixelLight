a064a0599297fb5dff38c5f118f622bf6d8b8397
Charlie Greenman <charie@razroo.com>
2016-05-29 06:10:31 -0400

    Add code box utils file.

---
+++ app/js/utils/code-box-utils.js
@@ -0,0 +1,8 @@
+export function compare(a, b){
+  if (parseFloat(a[0]) - parseFloat(b[0]) === 0) {
+    return parseFloat(a[1]) - parseFloat(b[1]);
+  }
+  else {
+    return parseFloat(a[0]) - parseFloat(b[0]);
+  }
+}
