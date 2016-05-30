957a94c9e08e34e88009b6d079ce5ef08ec7bdc1
Charlie Greenman <charie@razroo.com>
2016-05-30 11:11:10 -0400

    Add event listener for letter g toggler on toggle view componenet mount.

--- app/js/components/toggle-view.js
+++ app/js/components/toggle-view.js
@@ -14,10 +14,24 @@ class ToggleView extends React.Component {
     this.drawMode = this.drawMode.bind(this);
     this.reset    = this.reset.bind(this);
     this.toggle    = this.toggle.bind(this);
-    this.keyDownToggle    = this.keyDownToggle.bind(this);
+    this.onKeyDown    = this.onKeyDown.bind(this);
     this.state = {
       draw: true,
       view: false
+    }
+  }
+  componentDidMount() {
+    document.addEventListener('keydown', this.onKeyDown);
+  }
+  onKeyDown(e){
+    if(e.keyCode === 71){
+      const{dispatch} = this.props;
+      this.setState({
+        draw: !this.state.draw,
+        view: !this.state.view
+      },function(){
+          dispatch(InitDrawMode(this.state.draw, this.state.view));
+      });
     }
   }
   viewMode() {
@@ -48,17 +62,6 @@ class ToggleView extends React.Component {
         dispatch(InitDrawMode(this.state.draw, this.state.view));
     });
   }
-  keyDownToggle(e){
-    if(e.charCode === 71){
-      const{dispatch} = this.props;
-      this.setState({
-        draw: !this.state.draw,
-        view: !this.state.view
-      },function(){
-          dispatch(InitDrawMode(this.state.draw, this.state.view));
-      });
-    }
-  }
   reset(){
     const{dispatch} = this.props;
     dispatch(ResetApp());
@@ -70,7 +73,7 @@ class ToggleView extends React.Component {
             <div styleName="view-button" onClick={this.viewMode} >View Mode</div>
             <div styleName="draw-button" onClick={this.drawMode}>Draw Mode</div>
             <div styleName="reset-button" onClick={this.reset}>Reset</div>
-            <div styleName="color-button-styling" id="control_panel" onKeyDown = {this.keyDownToggle} onClick={this.toggle}>
+            <div styleName="color-button-styling" id="control_panel" onClick={this.toggle}>
               <span styleName="letter">G</span><span styleName="desc_text">Toggle View</span>
               {/*#github-button*/}
               {/*    a.github-button(href='https://github.com/charlieGreenman/codeIllustrator', data-icon='octicon-star', data-style='mega', data-count-href='/charlieGreenman/codeIllustrator/stargazers', data-count-api='/repos/charlieGreenman/codeIllustrator#stargazers_count', data-count-aria-label='# stargazers on GitHub', aria-label='Star charlieGreenman/codeIllustrator on GitHub') Star*/}
